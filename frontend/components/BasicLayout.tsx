69452adfdbb892cfd8b98b57b866ac756268bce7
hilariousz <andriano.abeh6@gmail.com>
2022-12-10 13:16:38 +0800

    feat: add local fonts with `@next/fonts`

--- frontend/components/BasicLayout.tsx
+++ frontend/components/BasicLayout.tsx
@@ -1,12 +1,49 @@
 import * as React from 'react'
-import { Inter } from '@next/font/google'
+import localFont from '@next/font/local'
 import Navigation from '@/components/Navigation'
 import Footer from '@/components/Footer'
 
-const inter = Inter({
+const inter = localFont({
+  src: [
+    {
+      path: '../fonts/Inter-Thin.ttf',
+      weight: '100',
+    },
+    {
+      path: '../fonts/Inter-ExtraLight.ttf',
+      weight: '200',
+    },
+    {
+      path: '../fonts/Inter-Light.ttf',
+      weight: '300',
+    },
+    {
+      path: '../fonts/Inter-Regular.ttf',
+      weight: '400',
+    },
+    {
+      path: '../fonts/Inter-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Inter-SemiBold.ttf',
+      weight: '600',
+    },
+    {
+      path: '../fonts/Inter-Bold.ttf',
+      weight: '700',
+    },
+    {
+      path: '../fonts/Inter-ExtraBold.ttf',
+      weight: '800',
+    },
+    {
+      path: '../fonts/Inter-Black.ttf',
+      weight: '900',
+    },
+  ],
   variable: '--font-inter',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'],
   display: 'swap',
 })
 
